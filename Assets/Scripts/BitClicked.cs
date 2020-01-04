aa1d2e72dc8fdef30b98d4818d23a28359219a72
Patrick <prm5983@rit.edu>
2020-01-04 15:16:49 -0500

    Began working on bit clicked script

---
+++ Assets/Scripts/BitClicked.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BitClicked : MonoBehaviour
+{
+    private bool isClicked;
+
+    private void OnMouseDown()
+    {
+        if (!isClicked)
+        {
+            isClicked = true;
+
+        }
+    }
+}
